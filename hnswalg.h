4ef22e9bfdd674ee6e7a800b5fd9b59a75770954
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-08-06 00:38:05 +0300

    .

--- hnswalg.h
+++ hnswalg.h
@@ -431,7 +431,7 @@ namespace hnswlib {
         cur_element_count++;
       }
       unique_lock<mutex> lock_el(ll_locks[cur_c]);
-      int curlevel = getRandomLevel(mult_);
+      int curlevel = 0; // Пока оставим один слой getRandomLevel(mult_);
       if (level > 0)
         curlevel = level;
       elementLevels[cur_c] = curlevel;
