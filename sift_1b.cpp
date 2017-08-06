fc9d2762583803b5e09ef6111ae1af98f3bec584
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-08-06 16:54:13 +0300

    .

--- sift_1b.cpp
+++ sift_1b.cpp
@@ -265,7 +265,7 @@ void printInfo(HierarchicalNSW<int> *hnsw)
 **/
 void sift_test1B()
 {
-	int subset_size_milllions = 20;
+	int subset_size_milllions = 50;
 	int efConstruction = 40;
 	int M = 16;
 
