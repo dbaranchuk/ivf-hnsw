7ddb3ef818d0edacca885433a652c32621429b26
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-08-08 15:01:22 +0300

    .

--- deep_10m.cpp
+++ deep_10m.cpp
@@ -274,7 +274,7 @@ void deep_test10M()
 	char *path_q = "/sata2/dbaranchuk/deep/deep1B_queries.fvecs";
 	char *path_data = "/sata2/dbaranchuk/deep/deep10M.fvecs";
 
-	sprintf(path_index, "/sata2/dbaranchuk/deep/deep10m_%dm_ef_%d_one_layer.bin", efConstruction, M);
+	sprintf(path_index, "/sata2/dbaranchuk/deep/deep10m_%dm_ef_%d_random.bin", efConstruction, M);
 	sprintf(path_gt,"/sata2/dbaranchuk/deep/deep10M_groundtruth1NN.ivecs");
 
 	float *massb = new float[vecdim];
@@ -314,7 +314,7 @@ void deep_test10M()
 	float *mass = new float[vecdim];
 	ifstream input(path_data, ios::binary);
 	int in = 0;
-	L2Space l2space(vecdim); // I
+	L2Space l2space(vecdim);
 
 	HierarchicalNSW<float> *appr_alg;
 	if (exists_test(path_index)) {
@@ -382,131 +382,3 @@ void deep_test10M()
 	cout << "Actual memory usage: " << getCurrentRSS() / 1000000 << " Mb \n";
 	return;
 }
-
-/**
- * Unsigned char
-*/
-//void deep_test1B()
-//{
-//    int subset_size_milllions = 10;
-//    int efConstruction = 40;
-//    int M = 16;
-//
-//    size_t vecsize = subset_size_milllions * 1000000;
-//    size_t qsize = 10000;
-//    size_t vecdim = 96;
-//
-//    char path_index[1024];
-//    char path_gt[1024];
-//    char *path_q = "/sata2/dbaranchuk/deep/deep1B_queries.bvecs";
-//    char *path_data = "/sata2/dbaranchuk/deep/deep10M.bvecs";
-//
-//    sprintf(path_index, "/sata2/dbaranchuk/deel10m_ef_%d_M_%d_random.bin", efConstruction, M);
-//    sprintf(path_gt,"/sata2/dbaranchuk/deep/idx_10M.ivecs");
-//
-//    unsigned char *massb = new unsigned char[vecdim];
-//
-//    cout << "Loading GT:\n";
-//    ifstream inputGT(path_gt, ios::binary);
-//    unsigned int *massQA = new unsigned int[qsize * 1000];
-//    for (int i = 0; i < qsize; i++) {
-//        int t;
-//        inputGT.read((char *)&t, 4);
-//        inputGT.read((char *)(massQA + 1000 * i), t * 4);
-//        if (t != 1000) {
-//            cout << "err";
-//            return;
-//        }
-//    }
-//
-//    cout << "Loading queries:\n";
-//    unsigned char *massQ = new unsigned char[qsize * vecdim];
-//    ifstream inputQ(path_q, ios::binary);
-//
-//    for (int i = 0; i < qsize; i++) {
-//        int in = 0;
-//        inputQ.read((char *)&in, 4);
-//        if (in != 96)
-//        {
-//            cout << "file error";
-//            exit(1);
-//        }
-//        inputQ.read((char *)massb, in);
-//        for (int j = 0; j < vecdim; j++) {
-//            massQ[i*vecdim + j] = massb[j];
-//        }
-//
-//    }
-//    inputQ.close();
-//
-//    unsigned char *mass = new unsigned char[vecdim];
-//    ifstream input(path_data, ios::binary);
-//    int in = 0;
-//    L2SpaceI l2space(vecdim);
-//
-//    HierarchicalNSW<int> *appr_alg;
-//    if (exists_test(path_index)) {
-//        cout << "Loading index from "<< path_index <<":\n";
-//        appr_alg=new HierarchicalNSW<int>(&l2space, path_index, false);
-//        cout << "Actual memory usage: " << getCurrentRSS() / 1000000 << " Mb \n";
-//    }
-//    else {
-//        cout << "Building index:\n";
-//        appr_alg = new HierarchicalNSW<int>(&l2space, vecsize, M, efConstruction);
-//
-//        input.read((char *)&in, 4);
-//        if (in != 96)
-//        {
-//            cout << "file error";
-//            exit(1);
-//        }
-//        input.read((char *)massb, in);
-//
-//        for (int j = 0; j < vecdim; j++) {
-//            mass[j] = massb[j] * (1.0f);
-//        }
-//
-//        appr_alg->addPoint((void *)(massb), (size_t)0);
-//        int j1 = 0;
-//        StopW stopw = StopW();
-//        StopW stopw_full = StopW();
-//        size_t report_every = 100000;
-//#pragma omp parallel for
-//        for (int i = 1; i < vecsize; i++) {
-//            unsigned char mass[96];
-//#pragma omp critical
-//            {
-//                input.read((char *)&in, 4);
-//                if (in != 96)
-//                {
-//                    cout << "file error";
-//                    exit(1);
-//                }
-//                input.read((char *)massb, in);
-//                for (int j = 0; j < vecdim; j++) {
-//                    mass[j] = massb[j];
-//                }
-//                j1++;
-//                if (j1 % report_every == 0) {
-//                    cout << j1 / (0.01*vecsize) << " %, " << report_every / (1000.0*1e-6*stopw.getElapsedTimeMicro()) << " kips " << " Mem: " << getCurrentRSS() / 1000000 << " Mb \n";
-//                    stopw.reset();
-//                }
-//            }
-//            appr_alg->addPoint((void *)(mass), (size_t)j1);
-//        }
-//        input.close();
-//        cout << "Build time:" << 1e-6*stopw_full.getElapsedTimeMicro() << "  seconds\n";
-//        appr_alg->SaveIndex(path_index);
-//    }
-//    printInfo(appr_alg);
-//
-//    vector<std::priority_queue< std::pair< int, labeltype >>> answers;
-//    size_t k = 1;
-//    cout << "Parsing gt:\n";
-//    get_gt(massQA, massQ, mass, vecsize, qsize, l2space, vecdim, answers, k);
-//    cout << "Loaded gt\n";
-//    for (int i = 0; i < 1; i++)
-//        test_vs_recall(massQ, vecsize, qsize, *appr_alg, vecdim, answers, k);
-//    cout << "Actual memory usage: " << getCurrentRSS() / 1000000 << " Mb \n";
-//    return;
-//}
