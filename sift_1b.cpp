5b67554179a1adbeea972d81249ec43e695b8688
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-08-10 01:33:31 +0300

    .

--- sift_1b.cpp
+++ sift_1b.cpp
@@ -280,7 +280,7 @@ void sift_test1B()
     char *path_data = "/sata2/dbaranchuk/synthetic_1m/sift_synthetic.bvecs";
 
     sprintf(path_index, "/sata2/dbaranchuk/synthetic_1m/sift1m_ef_%d_M_%d_synthetic.bin", efConstruction, M);
-    sprintf(path_gt,"/sata2/dbaranchuk/synthetic_1m/idx_1M.ivecs");
+    sprintf(path_gt,"/sata2/dbaranchuk/synthetic_1m/sift_groundtruth.ivecs");
     //char *path_q = "/sata2/dbaranchuk/bigann/bigann_query.bvecs";
 	//char *path_data = "/sata2/dbaranchuk/bigann/bigann_base.bvecs";
 
