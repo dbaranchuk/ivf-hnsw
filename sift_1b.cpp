6ca4c6a8e3e9d8ee828f3308f5157751b24bd11d
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-08-13 02:13:37 +0300

    .

--- sift_1b.cpp
+++ sift_1b.cpp
@@ -279,7 +279,7 @@ void sift_test1B()
     char *path_q = "/sata2/dbaranchuk/synthetic_100m_100k/bigann_query.bvecs";
     char *path_data = "/sata2/dbaranchuk/synthetic_100m_100k/bigann_synthetic_100m_100k.bvecs";
 
-    sprintf(path_index, "/sata2/dbaranchuk/synthetic_100m_100k/sift100m_ef_%d_M_%d_hnsw.bin", efConstruction, M);
+    sprintf(path_index, "/sata2/dbaranchuk/synthetic_100m_100k/sift100m_ef_%d_M_%d_smart.bin", efConstruction, M);
     sprintf(path_gt,"/sata2/dbaranchuk/synthetic_100m_100k/idx_100M.ivecs");
     //char *path_q = "/sata2/dbaranchuk/bigann/bigann_query.bvecs";
 	//char *path_data = "/sata2/dbaranchuk/bigann/bigann_base.bvecs";
@@ -349,7 +349,7 @@ void sift_test1B()
 			mass[j] = massb[j] * (1.0f);
 		}
 
-		appr_alg->addPoint((void *)(massb), (size_t)0); // не было третьего параметра
+		appr_alg->addPoint((void *)(massb), (size_t)0, 2); // не было третьего параметра
 		int j1 = 0;
 		StopW stopw = StopW();
 		StopW stopw_full = StopW();
@@ -385,7 +385,7 @@ void sift_test1B()
                 level = 1;
             else
                 level = 0;
-            appr_alg->addPoint((void *)(mass), (size_t)j1);
+            appr_alg->addPoint((void *)(mass), (size_t)j1, level);
 		}
 		input.close();
 		cout << "Build time:" << 1e-6*stopw_full.getElapsedTimeMicro() << "  seconds\n";
