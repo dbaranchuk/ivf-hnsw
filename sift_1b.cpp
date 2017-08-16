a3e68c562eea9a69d8558370631f7b374c0d572f
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-08-16 13:23:25 +0300

    .

--- sift_1b.cpp
+++ sift_1b.cpp
@@ -396,9 +396,9 @@ void sift_test1B()
                 level = 1;
             else {
                 level = 0;
-                appr_alg->M_ = 1;
-                appr_alg->maxM0_ = 2;
-                appr_alg->maxM_ = 2;
+                appr_alg->M_ = 4;
+                appr_alg->maxM0_ = 8;
+                appr_alg->maxM_ = 8;
             }
             appr_alg->addPoint((void *)(mass), (size_t)j1, level);
 		}
