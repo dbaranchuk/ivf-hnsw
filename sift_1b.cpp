9a8e331a8e43087ced4472f6fc42de2a3da235b8
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-08-21 13:02:55 +0300

    .

--- sift_1b.cpp
+++ sift_1b.cpp
@@ -278,7 +278,7 @@ void sift_test1B()
 	int subset_size_milllions = 100;
 	int efConstruction = 60;
 	int M = 2;
-    int M_cluster = 2;
+    int M_cluster = 16;
 
     size_t clustersize = 5263157;
     const vector<size_t> elements_per_layer = {100000000, 5000000, 250000, 12500, 625, 32};
