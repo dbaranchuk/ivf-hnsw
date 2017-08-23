066a486322ca600f80ac6b33dd591ebff47bd448
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-08-24 00:50:34 +0300

    .

--- sift_1b.cpp
+++ sift_1b.cpp
@@ -328,13 +328,23 @@ void sift_test1B()
     } else {
 		cout << "Building index:\n";
 		appr_alg = new HierarchicalNSW<int>(&l2space, vecsize, M, efConstruction, clustersize, M_cluster);
+
+		input.read((char *)&in, 4);
+		if (in != vecdim)
+		{
+			cout << "file error";
+			exit(1);
+		}
+		input.read((char *)massb, in);
+
+		appr_alg->addPoint((void *)(massb), (size_t)0, 5); // не было третьего параметра
 		int j1 = 0;
 		StopW stopw = StopW();
 		StopW stopw_full = StopW();
 		size_t report_every = 1000000;
 #pragma omp parallel for
-		for (int i = 0; i < vecsize + clustersize; i++) {
-			unsigned char mass[128];
+		for (int i = 1; i < vecsize + clustersize; i++) {
+			unsigned char massb[128];
 #pragma omp critical
 			{
 				input.read((char *)&in, 4);
@@ -343,7 +353,7 @@ void sift_test1B()
 					cout << "file error";
 					exit(1);
 				}
-				input.read((char *)mass, in);
+				input.read((char *)massb, in);
 				j1++;
 				if (j1 % report_every == 0) {
                     cout << j1 / (0.01 * (vecsize + clustersize)) << " %, "
