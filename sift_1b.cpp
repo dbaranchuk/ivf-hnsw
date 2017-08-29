22342f6f3cf3456f3b4b44b0f8d32c37d8776c8a
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-08-29 23:44:18 +0300

    .

--- sift_1b.cpp
+++ sift_1b.cpp
@@ -406,7 +406,7 @@ void sift_test1B() {
 
 void sift_test1B_PQ()
 {
-    const int subset_size_milllions = 100;
+    const int subset_size_milllions = 200;
     const int efConstruction = 200;
     const int M = 16;
     const int M_PQ = 16;
