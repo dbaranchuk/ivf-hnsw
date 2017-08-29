e96f5c472e979d00b5ae893c0140026641659d02
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-08-29 14:01:12 +0300

    .

--- L2space.h
+++ L2space.h
@@ -344,7 +344,7 @@ namespace hnswlib {
 
                 for (int j = 0; j < vocab_dim_; j++) {
                     float t = x[j] - y[j];
-                    std::cout << (float)(x[j]) << " " << y[j] << " " << t * t << "\n";
+                    std::cout << (int)(x[j]) << " " << y[j] << " " << t * t << "\n";
                     res += t * t;
                 }
             }
