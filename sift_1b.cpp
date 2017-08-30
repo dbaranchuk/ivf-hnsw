0a65df111704941219a6d182e667cb6fd5824df8
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-08-30 18:48:29 +0300

    .

--- sift_1b.cpp
+++ sift_1b.cpp
@@ -338,57 +338,57 @@ void sift_test1B() {
         StopW stopw = StopW();
         StopW stopw_full = StopW();
 
-        cout << "Adding clustets:\n";
-        ifstream inputC(path_clusters, ios::binary);
-        inputC.read((char *) &in, 4);
-        if (in != vecdim) {
-            cout << "file error\n";
-            exit(1);
-        }
-        inputC.read((char *) massb, in);
-        appr_alg->addPoint((void *) (massb), (size_t) j1, level);
-
-#pragma omp parallel for
-        for (int i = 1; i < clustersize; i++) {
-            unsigned char massb[vecdim];
-#pragma omp critical
-            {
-                inputC.read((char *) &in, 4);
-                if (in != vecdim) {
-                    cout << "file error";
-                    exit(1);
-                }
-                inputC.read((char *) massb, in);
-                j1++;
-            }
-            if (j1 < elements_per_layer[5])
-                level = 5;
-            else if (j1 < elements_per_layer[5] + elements_per_layer[4])
-                level = 4;
-            else if (j1 < elements_per_layer[5] + elements_per_layer[4] + elements_per_layer[3])
-                level = 3;
-            else if (j1 < elements_per_layer[5] + elements_per_layer[4] + elements_per_layer[3] + elements_per_layer[2])
-                level = 2;
-            else if (j1 < clustersize)
-                level = 1;
-
-            appr_alg->addPoint((void *) (massb), (size_t) j1, level);
-        }
-        inputC.close();
-        cout << "Clusters have been added" << endl;
+//        cout << "Adding clustets:\n";
+//        ifstream inputC(path_clusters, ios::binary);
+//        inputC.read((char *) &in, 4);
+//        if (in != vecdim) {
+//            cout << "file error\n";
+//            exit(1);
+//        }
+//        inputC.read((char *) massb, in);
+//        appr_alg->addPoint((void *) (massb), (size_t) j1, level);
+//
+//#pragma omp parallel for
+//        for (int i = 1; i < clustersize; i++) {
+//            unsigned char massb[vecdim];
+//#pragma omp critical
+//            {
+//                inputC.read((char *) &in, 4);
+//                if (in != vecdim) {
+//                    cout << "file error";
+//                    exit(1);
+//                }
+//                inputC.read((char *) massb, in);
+//                j1++;
+//            }
+//            if (j1 < elements_per_layer[5])
+//                level = 5;
+//            else if (j1 < elements_per_layer[5] + elements_per_layer[4])
+//                level = 4;
+//            else if (j1 < elements_per_layer[5] + elements_per_layer[4] + elements_per_layer[3])
+//                level = 3;
+//            else if (j1 < elements_per_layer[5] + elements_per_layer[4] + elements_per_layer[3] + elements_per_layer[2])
+//                level = 2;
+//            else if (j1 < clustersize)
+//                level = 1;
+//
+//            appr_alg->addPoint((void *) (massb), (size_t) j1, level);
+//        }
+//        inputC.close();
+//        cout << "Clusters have been added" << endl;
 
         cout << "Adding elements\n";
         level = 0;
         ifstream input(path_data, ios::binary);
         //
-//        input.read((char *) &in, 4);
-//        if (in != vecdim) {
-//            cout << "file error\n";
-//            exit(1);
-//        }
-//        input.read((char *) massb, in);
-//
-//        appr_alg->addPoint((void *) (massb), (size_t) j1, level);
+        input.read((char *) &in, 4);
+        if (in != vecdim) {
+            cout << "file error\n";
+            exit(1);
+        }
+        input.read((char *) massb, in);
+
+        appr_alg->addPoint((void *) (massb), (size_t) j1);
         //
         size_t report_every = 1000000;
 #pragma omp parallel for
@@ -410,7 +410,7 @@ void sift_test1B() {
                     stopw.reset();
                 }
             }
-            appr_alg->addPoint((void *) (massb), (size_t) j1, level);
+            appr_alg->addPoint((void *) (massb), (size_t) j1);//, level);
         }
         input.close();
         cout << "Build time:" << 1e-6 * stopw_full.getElapsedTimeMicro() << "  seconds\n";
