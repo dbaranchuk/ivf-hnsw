dae2d9e4f0eb64861eda609fec034913b378f894
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-09-02 19:44:33 +0300

    .

--- sift_1b.cpp
+++ sift_1b.cpp
@@ -433,7 +433,7 @@ void sift_test1B() {
 void sift_test1B_PQ()
 {
     const int subset_size_milllions = 1000;
-    const int efConstruction = 200;
+    const int efConstruction = 240;
     const int M = 4;
     const int M_PQ = 16;
     const int M_cluster = 0;
