fb0a5c0d5f3f02122d5146bb09e037ce910bbb71
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-09-02 19:10:50 +0300

    .

--- sift_1b.cpp
+++ sift_1b.cpp
@@ -270,12 +270,12 @@ static void printInfo(HierarchicalNSW<dist_t> *hnsw)
  * Main SIFT Test Function
 */
 void sift_test1B() {
-    const int subset_size_milllions = 100;
+    const int subset_size_milllions = 10;
     const int efConstruction = 240;
     const int M = 16;
-    const int M_cluster = 16;
+    const int M_cluster = 0;//16;
 
-    const size_t clustersize = 5263157;
+    const size_t clustersize = 0;//5263157;
     const vector<size_t> elements_per_layer = {100000000, 5000000, 250000, 12500, 625, 32};
 
     const size_t vecsize = subset_size_milllions * 1000000;
