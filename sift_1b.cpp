b8b881aaed841ef2cdf08c5dcac5e66baaf2eab8
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-09-06 23:50:52 +0300

    .

--- sift_1b.cpp
+++ sift_1b.cpp
@@ -565,7 +565,7 @@ void sift_test1B_PQ()
                 input.read((char *) massb, in);
                 j1++;
                 if (j1 % report_every == 0) {
-                    cout << j1 / (0.01 * vecsize) << " %, "
+                    cout << j1 / (0.01 * (clustersize + vecsize)) << " %, "
                          << report_every / (1000.0 * 1e-6 * stopw.getElapsedTimeMicro()) << " kips " << " Mem: "
                          << getCurrentRSS() / 1000000 << " Mb \n";
                     stopw.reset();
