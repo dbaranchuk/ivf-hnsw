debec5980e3cfd83a595673ee569bbf2c5c51c1e
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-09-19 19:43:31 +0300

    .

--- sift_1b.cpp
+++ sift_1b.cpp
@@ -413,9 +413,9 @@ static void _hnsw_test(const char *path_codebooks, const char *path_tables, cons
     const int M_PQ = 16;
     const bool PQ = (path_codebooks && path_tables);
 
-    const int specsize = 100000000;//101917929;
-    const map<size_t, pair<size_t, size_t>> M_map = {{vecsize, {M, 2*M}}};
-    //const map<size_t, size_t> M_map = {{specsize, 6}, {vecsize-specsize, M}, {vecsize, M}};
+    const int specsize = 10000000;//101917929;
+    //const map<size_t, pair<size_t, size_t>> M_map = {{vecsize, {M, 2*M}}};
+    const map<size_t, pair<size_t, size_t>> M_map = {{specsize, {16, 32}}, {vecsize-specsize, {M, 2*M}}, {vecsize, {M, 2*M}}};
     //const map<size_t, size_t> M_map = {{50000000, 32}, {100000000, 24}, {150000000, 16}, {800000000, 8}, {900000000, 6}, {1000000000, 4}};
     //const map<size_t, pair<size_t, size_t>> M_map = {{100000000, {16, 32}},{200000000, {10, 20}},{400000000, {5, 10}},
     //                                                 {600000000, {5, 9}},{800000000, {5, 8}},{900000000, {5, 7}},{vecsize, {5, 6}}};
