5202fc7505f3d08bcef088eacf464c8c553bd947
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-09-25 01:58:40 +0300

    .

--- sift_1b.cpp
+++ sift_1b.cpp
@@ -226,6 +226,8 @@ static void test_vs_recall(vtype *massQ, size_t qsize, HierarchicalNSW<dist_t, v
         appr_alg.nev9zka = 0.0;
         appr_alg.hops = 0.0;
         appr_alg.hops0 = 0.0;
+
+        k = 1;
 		StopW stopw = StopW();
 		float recall = test_approx<dist_t, vtype>(massQ, qsize, appr_alg, vecdim, answers, k, cluster_idx_set, pq);
 		float time_us_per_query = stopw.getElapsedTimeMicro() / qsize;
