adc3a0d6bfc1146f91a1aa4b3a6c5c040bfee337
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-09-26 07:47:26 +0300

    .

--- hnswalg.h
+++ hnswalg.h
@@ -876,7 +876,6 @@ namespace hnswlib {
             double gain = 0.0;
             size_t n1 = v1.size(), n2 = v2.size();
 
-            //#pragma omp parallel for num_threads(4)
             for (int i = 0; i < maxelements_; i++) {
                 linklistsizeint *ll_cur = get_linklist0(i);
                 size_t size = *ll_cur;
@@ -919,7 +918,7 @@ namespace hnswlib {
             priority_queue<std::pair<double, labeltype>> s1, s2;
             cout << "Compute Move Gains" << endl;
 
-            //#pragma omp parallel for num_threads(4)
+            #pragma omp parallel for num_threads(4)
             for (int i = 0; i < n1; i++)
                 s1.push(std::pair<double, labeltype>(computeMoveGain(*(start + i), error, v1, v2, true), *(start + i)));
 
