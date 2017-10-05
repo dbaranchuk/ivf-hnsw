0811523ffb538f90c1b7a77265e3b41680dc454d
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-10-05 13:57:49 +0300

    .

--- hnswIndexPQ.h
+++ hnswIndexPQ.h
@@ -100,7 +100,7 @@ namespace hnswlib {
 
 		void assign(const char *path_base, idx_t *precomputed_idx, size_t vecsize)
 		{
-            quantizer->ef_ = 100;
+            quantizer->ef_ = 240;
 			std::cout << "Assigning base elements\n";
 			int j1 = 0;
 			std::ifstream input(path_base, ios::binary);
