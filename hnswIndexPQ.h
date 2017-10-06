f0ae125c32b55de8019c1289d09cf3b6a5b2891d
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-10-06 11:08:45 +0300

    .

--- hnswIndexPQ.h
+++ hnswIndexPQ.h
@@ -53,7 +53,7 @@ namespace hnswlib {
         std::vector < std::vector<idx_t> > ids;
 
 		/** Query members **/
-		size_t nprobe = 32;
+		size_t nprobe = 128;
         size_t max_codes = 10000;
 
 		float *dis_tables;
