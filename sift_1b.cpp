ef3e89b41a11bdedecb8ae288fdbb356ef958794
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-10-06 18:12:29 +0300

    .

--- sift_1b.cpp
+++ sift_1b.cpp
@@ -226,7 +226,7 @@ static void test_vs_recall(vtype *massQ, size_t qsize, HierarchicalNSW<dist_t, v
         appr_alg.hops0 = 0.0;
 		StopW stopw = StopW();
         if (pq)
-            dynamic_cast<L2SpacePQ *>(appr_alg.space)->compute_query_tables(massQ, qsize);
+            dynamic_cast<L2SpacePQ *>(appr_alg.space)->compute_query_tables((unsigned char *)massQ, qsize);
 
 		float recall = test_approx<dist_t, vtype>(massQ, qsize, appr_alg, vecdim, answers, k, pq);
 		float time_us_per_query = stopw.getElapsedTimeMicro() / qsize;
