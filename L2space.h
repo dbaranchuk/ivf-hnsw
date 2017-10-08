9dca42301a80284fdad4577472394c5bb475683f
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-10-08 14:52:44 +0300

    .

--- L2space.h
+++ L2space.h
@@ -17,6 +17,8 @@
 
 #include <cmath>
 #include "hnswlib.h"
+#include <faiss/utils.h>
+
 namespace hnswlib {
 	using namespace std;
     /**= Prev fstdistfunc =**/
@@ -149,81 +151,86 @@ namespace hnswlib {
 		size_t get_data_size() { return data_size_; }
         size_t get_data_dim() { return dim_; }
 
+//        float fstdistfunc(const void *x, const void *y) {
+//            float *pVect1 = (float *) x;
+//            float *pVect2 = (float *) y;
+//            float PORTABLE_ALIGN32 TmpRes[8];
+//            #ifdef USE_AVX
+//            size_t qty16 = dim_ >> 4;
+//
+//            const float *pEnd1 = pVect1 + (qty16 << 4);
+//
+//            __m256 diff, v1, v2;
+//            __m256 sum = _mm256_set1_ps(0);
+//
+//            while (pVect1 < pEnd1) {
+//                v1 = _mm256_loadu_ps(pVect1);
+//                pVect1 += 8;
+//                v2 = _mm256_loadu_ps(pVect2);
+//                pVect2 += 8;
+//                diff = _mm256_sub_ps(v1, v2);
+//                sum = _mm256_add_ps(sum, _mm256_mul_ps(diff, diff));
+//
+//                v1 = _mm256_loadu_ps(pVect1);
+//                pVect1 += 8;
+//                v2 = _mm256_loadu_ps(pVect2);
+//                pVect2 += 8;
+//                diff = _mm256_sub_ps(v1, v2);
+//                sum = _mm256_add_ps(sum, _mm256_mul_ps(diff, diff));
+//            }
+//
+//            _mm256_store_ps(TmpRes, sum);
+//            float res = TmpRes[0] + TmpRes[1] + TmpRes[2] + TmpRes[3] + TmpRes[4] + TmpRes[5] + TmpRes[6] + TmpRes[7];
+//
+//            return (res);
+//            #else
+//            size_t qty16 = dim_ >> 4;
+//
+//            const float *pEnd1 = pVect1 + (qty16 << 4);
+//
+//            __m128 diff, v1, v2;
+//            __m128 sum = _mm_set1_ps(0);
+//
+//            while (pVect1 < pEnd1) {
+//                v1 = _mm_loadu_ps(pVect1);
+//                pVect1 += 4;
+//                v2 = _mm_loadu_ps(pVect2);
+//                pVect2 += 4;
+//                diff = _mm_sub_ps(v1, v2);
+//                sum = _mm_add_ps(sum, _mm_mul_ps(diff, diff));
+//
+//                v1 = _mm_loadu_ps(pVect1);
+//                pVect1 += 4;
+//                v2 = _mm_loadu_ps(pVect2);
+//                pVect2 += 4;
+//                diff = _mm_sub_ps(v1, v2);
+//                sum = _mm_add_ps(sum, _mm_mul_ps(diff, diff));
+//
+//                v1 = _mm_loadu_ps(pVect1);
+//                pVect1 += 4;
+//                v2 = _mm_loadu_ps(pVect2);
+//                pVect2 += 4;
+//                diff = _mm_sub_ps(v1, v2);
+//                sum = _mm_add_ps(sum, _mm_mul_ps(diff, diff));
+//
+//                v1 = _mm_loadu_ps(pVect1);
+//                pVect1 += 4;
+//                v2 = _mm_loadu_ps(pVect2);
+//                pVect2 += 4;
+//                diff = _mm_sub_ps(v1, v2);
+//                sum = _mm_add_ps(sum, _mm_mul_ps(diff, diff));
+//            }
+//            _mm_store_ps(TmpRes, sum);
+//            float res = TmpRes[0] + TmpRes[1] + TmpRes[2] + TmpRes[3];
+//
+//            return (res);
+//            #endif
+//        }
+
         float fstdistfunc(const void *x, const void *y) {
-            float *pVect1 = (float *) x;
-            float *pVect2 = (float *) y;
-            float PORTABLE_ALIGN32 TmpRes[8];
-            #ifdef USE_AVX
-            size_t qty16 = dim_ >> 4;
-
-            const float *pEnd1 = pVect1 + (qty16 << 4);
-
-            __m256 diff, v1, v2;
-            __m256 sum = _mm256_set1_ps(0);
-
-            while (pVect1 < pEnd1) {
-                v1 = _mm256_loadu_ps(pVect1);
-                pVect1 += 8;
-                v2 = _mm256_loadu_ps(pVect2);
-                pVect2 += 8;
-                diff = _mm256_sub_ps(v1, v2);
-                sum = _mm256_add_ps(sum, _mm256_mul_ps(diff, diff));
-
-                v1 = _mm256_loadu_ps(pVect1);
-                pVect1 += 8;
-                v2 = _mm256_loadu_ps(pVect2);
-                pVect2 += 8;
-                diff = _mm256_sub_ps(v1, v2);
-                sum = _mm256_add_ps(sum, _mm256_mul_ps(diff, diff));
-            }
-
-            _mm256_store_ps(TmpRes, sum);
-            float res = TmpRes[0] + TmpRes[1] + TmpRes[2] + TmpRes[3] + TmpRes[4] + TmpRes[5] + TmpRes[6] + TmpRes[7];
-
-            return (res);
-            #else
-            size_t qty16 = dim_ >> 4;
-
-            const float *pEnd1 = pVect1 + (qty16 << 4);
-
-            __m128 diff, v1, v2;
-            __m128 sum = _mm_set1_ps(0);
-
-            while (pVect1 < pEnd1) {
-                v1 = _mm_loadu_ps(pVect1);
-                pVect1 += 4;
-                v2 = _mm_loadu_ps(pVect2);
-                pVect2 += 4;
-                diff = _mm_sub_ps(v1, v2);
-                sum = _mm_add_ps(sum, _mm_mul_ps(diff, diff));
-
-                v1 = _mm_loadu_ps(pVect1);
-                pVect1 += 4;
-                v2 = _mm_loadu_ps(pVect2);
-                pVect2 += 4;
-                diff = _mm_sub_ps(v1, v2);
-                sum = _mm_add_ps(sum, _mm_mul_ps(diff, diff));
-
-                v1 = _mm_loadu_ps(pVect1);
-                pVect1 += 4;
-                v2 = _mm_loadu_ps(pVect2);
-                pVect2 += 4;
-                diff = _mm_sub_ps(v1, v2);
-                sum = _mm_add_ps(sum, _mm_mul_ps(diff, diff));
-
-                v1 = _mm_loadu_ps(pVect1);
-                pVect1 += 4;
-                v2 = _mm_loadu_ps(pVect2);
-                pVect2 += 4;
-                diff = _mm_sub_ps(v1, v2);
-                sum = _mm_add_ps(sum, _mm_mul_ps(diff, diff));
-            }
-            _mm_store_ps(TmpRes, sum);
-            float res = TmpRes[0] + TmpRes[1] + TmpRes[2] + TmpRes[3];
-
-            return (res);
-            #endif
+            return faiss::fvec_L2sqr ((float *) x, (float *) y, dim_);
         }
+
         float fstdistfuncST(const void *y_code) { return 0.0; }
 	};
 
