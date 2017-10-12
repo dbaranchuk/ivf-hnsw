3c7d9e8744395fde268b627a91c858498d4a7e06
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-10-13 01:23:55 +0300

    .

--- hnswalg.h
+++ hnswalg.h
@@ -799,7 +799,7 @@ namespace hnswlib {
                 for (int j = 0; j < D; j++)
                     massf[j] = (1.0)*mass[j];
 
-                //dynamic_cast<NewL2SpacePQ *>(space)->pq->compute_code(massf, mass_code);
+                dynamic_cast<NewL2SpacePQ<dist_t> *>(space)->pq->compute_code(massf, mass_code);
 
                 // Initialisation of the data and label
                 //memcpy(getExternalLabelPointer(cur_c), &label, sizeof(labeltype));
