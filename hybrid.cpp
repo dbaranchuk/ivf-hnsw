210b7cdde44c9de64a9fe304489a66d056d0dd2e
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-10-18 01:17:07 +0300

    .

--- hybrid.cpp
+++ hybrid.cpp
@@ -195,7 +195,7 @@ static void loadXvecs(const char *path, format *mass, const int n, const int d)
 }
 
 static void check_precomputing(Index *index, const char *path_data, const char *path_precomputed_idxs,
-                               size_t vecdim, size_t ncentroids, size_t vecsize, std::set<int> gt_mistakes)
+                               size_t vecdim, size_t ncentroids, size_t vecsize, std::set<size_t> gt_mistakes)
 {
     size_t batch_size = 1000000;
     std::ifstream base_input(path_data, ios::binary);
