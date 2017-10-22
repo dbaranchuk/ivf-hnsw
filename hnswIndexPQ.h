4d9b19af35ad97b8f2c3a5fe7c010da7347473b0
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-10-23 00:30:33 +0300

    .

--- hnswIndexPQ.h
+++ hnswIndexPQ.h
@@ -237,8 +237,8 @@ namespace hnswlib {
                 dis_table = new float [pq->ksub * pq->M];
 
             pq->compute_inner_prod_table(x, dis_table);
-            //std::priority_queue<std::pair<float, idx_t>, std::vector<pair<float, idx_t>>, CompareByFirst> topResults;
-            std::priority_queue<std::pair<float, idx_t>> topResults;
+            std::priority_queue<std::pair<float, idx_t>, std::vector<pair<float, idx_t>>, CompareByFirst> topResults;
+            //std::priority_queue<std::pair<float, idx_t>> topResults;
 
             auto coarse = quantizer->searchKnn(x, nprobe);
 
