fa2cb57344a3ec0e057820abcf9add229ada7f2f
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-10-24 14:02:42 +0300

    .

--- hybrid.cpp
+++ hybrid.cpp
@@ -373,7 +373,7 @@ void check_idea(Index *index, const char *path_centroids,
 
     std::ifstream input(path_groups, ios::binary);
 
-    const int ngroups = 10000;
+    const int ngroups = 1;//0000;
     for (int g = 0; g < ngroups; g++) {
 
         /** Read Original vectors from Group file**/
@@ -383,6 +383,8 @@ void check_idea(Index *index, const char *path_centroids,
         input.read((char *)&centroid_num, sizeof(idx_t));
         input.read((char *)&groupsize, sizeof(int));
 
+        std::cout << centroid_num << " " << groupsize << std::endl;
+        
         std::vector<float> data(groupsize * vecdim);
         readXvecs<float>(input, data.data(), vecdim, groupsize);
 
