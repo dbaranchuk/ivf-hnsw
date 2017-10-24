36c4e28bed16b6a87f356de0b89910d7452747b3
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-10-24 21:18:10 +0300

    .

--- hybrid.cpp
+++ hybrid.cpp
@@ -513,7 +513,7 @@ void check_idea(Index *index, const char *path_centroids,
             input.read((char *) &centroid_num, sizeof(idx_t));
             input.read((char *) &groupsize, sizeof(int));
             //std::cout << centroid_num << " " << groupsize << std::endl;
-            data.reverse(groupsize * vecdim);
+            data.reserve(groupsize * vecdim);
             readXvecs<float>(input, data.data(), vecdim, groupsize);
         }
 
