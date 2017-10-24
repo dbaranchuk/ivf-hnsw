21ef55d2431ebc7b028f5154914ee64c860f23a3
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-10-24 18:33:13 +0300

    .

--- hybrid.cpp
+++ hybrid.cpp
@@ -440,7 +440,7 @@ void check_idea(Index *index, const char *path_centroids,
                 const int vecsize, const int vecdim)
 {
     const bool include_zero_centroid = false;
-    const int nc = 16;
+    const int nc = 32;
     const char *path_groups = "groups1M_10000.bin";
 
     if (!exists_test(path_groups)) {
