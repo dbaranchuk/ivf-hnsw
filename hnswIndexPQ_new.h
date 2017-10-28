594724a0017c95a2a9d531355258b1e9298af096
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-10-28 05:15:27 +0300

    .

--- hnswIndexPQ_new.h
+++ hnswIndexPQ_new.h
@@ -151,7 +151,7 @@ namespace hnswlib {
             /** Find NN centroids to source centroid **/
             std::cout << "Find NN centroids to source centroids\n";
 
-            //#pragma omp parallel for num_threads(24)
+            #pragma omp parallel for num_threads(20)
             for (int i = 0; i < nc; i++) {
                 const float *centroid = (float *) quantizer->getDataByInternalId(i);
                 std::priority_queue<std::pair<float, idx_t>> nn_centroids_raw = quantizer->searchKnn((void *) centroid, nsubc + 1);
