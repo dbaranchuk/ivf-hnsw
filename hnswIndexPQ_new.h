ba9737556a6c4babb63f187f0735f436743d1918
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-11-04 10:13:56 +0300

    .

--- hnswIndexPQ_new.h
+++ hnswIndexPQ_new.h
@@ -100,7 +100,8 @@ namespace hnswlib {
                 quantizer->ef_ = efSearch;
                 return;
             }
-            quantizer = new HierarchicalNSW<float, float>(l2space, nc, 16, 32, 500);
+            quantizer = new HierarchicalNSW<float, float> (l2space, {{nc, {16, 32}}}, 500);
+            //quantizer = new HierarchicalNSW<float, float>(l2space, nc, 16, 32, 500);
             quantizer->ef_ = efSearch;
 
             std::cout << "Constructing quantizer\n";
