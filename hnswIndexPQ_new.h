625ed10c2ee7bb0fd2bb12fad53a785c11e2fdb2
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-11-05 12:24:57 +0300

    .

--- hnswIndexPQ_new.h
+++ hnswIndexPQ_new.h
@@ -365,7 +365,7 @@ namespace hnswlib {
                     idx_t subcentroid_num = nn_centroids[subc];
                     const float *nn_centroid = (float *) quantizer->getDataByInternalId(subcentroid_num);
                     float q_s = faiss::fvec_L2sqr(x, nn_centroid, d);
-                    float snd_term = alpha * (q_s[subc] - centroid_norms[subcentroid_num]);
+                    float snd_term = alpha * (q_s - centroid_norms[subcentroid_num]);
 
                     //size_t offset = offsets[subc];
                     for (int j = 0; j < groupsize; j++){
