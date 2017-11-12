aba8355474916109bdb0bdef09b41b45a639e07e
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-11-12 17:59:02 +0300

    .

--- hnswIndexPQ_new.h
+++ hnswIndexPQ_new.h
@@ -357,7 +357,7 @@ namespace hnswlib {
                     continue;
 
                 /** Threshold **/
-                int threshold = (ncode < 4096) ? 48 : 32;
+                int threshold = (ncode < 2048) ? 48 : 32;
 
                 const idx_t *groupsizes = group_sizes[centroid_num].data();
                 uint8_t *groupcodes = codes[centroid_num].data();
