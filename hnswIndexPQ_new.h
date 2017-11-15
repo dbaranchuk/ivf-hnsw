aaad1d5362fb74056051faf09146311287c0cef5
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-11-15 16:18:35 +0300

    .

--- hnswIndexPQ_new.h
+++ hnswIndexPQ_new.h
@@ -1152,7 +1152,7 @@ namespace hnswlib {
                 r_threshold /= normalize;
 
                 double r_threshold_25 = 0.0;
-                while (r_ordered.size() > 0.75*normalize){
+                while (r_ordered.size() > 0.5*normalize){
                     r_threshold_25 = -r_ordered.top().first;
                     r_ordered.pop();
                 }
