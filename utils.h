8943da1db27e71c8a44d64e8bc7b7d36b5a01850
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-12-15 13:50:06 +0300

    .

--- utils.h
+++ utils.h
@@ -177,6 +177,6 @@ void random_subset(const float *x, float *x_out, int d, int nx, int sub_nx)
     std::vector<int> perm (nx);
     faiss::rand_perm (perm.data (), nx, seed);
 
-    for (idx_t i = 0; i < sub_nx; i++)
+    for (int i = 0; i < sub_nx; i++)
         memcpy (x_out + i * d, x + perm[i] * d, sizeof(x_out[0]) * d);
 }
