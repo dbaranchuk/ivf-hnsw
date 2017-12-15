efcaee7c88a17b91c1cae51f6c2e5cf4d8a164d1
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-12-15 19:52:30 +0300

    .

---
+++ utils.cpp
@@ -0,0 +1,14 @@
+//
+// Created by dbaranchuk on 15.12.17.
+//
+
+#include "utils.h"
+
+void random_subset(const float *x, float *x_out, int d, int nx, int sub_nx) {
+    int seed = 1234;
+    std::vector<int> perm(nx);
+    faiss::rand_perm(perm.data(), nx, seed);
+
+    for (int i = 0; i < sub_nx; i++)
+        memcpy(x_out + i * d, x + perm[i] * d, sizeof(x_out[0]) * d);
+}
--- utils.h
+++ utils.h
@@ -56,14 +56,7 @@ void readXvecFvec(std::ifstream &input, float *data, const int d, const int n = 
     }
 }
 
-void random_subset(const float *x, float *x_out, int d, int nx, int sub_nx) {
-    int seed = 1234;
-    std::vector<int> perm(nx);
-    faiss::rand_perm(perm.data(), nx, seed);
-
-    for (int i = 0; i < sub_nx; i++)
-        memcpy(x_out + i * d, x + perm[i] * d, sizeof(x_out[0]) * d);
-}
+void random_subset(const float *x, float *x_out, int d, int nx, int sub_nx);
 
 
 enum class Dataset
