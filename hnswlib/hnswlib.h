7291e2d29af841f2866794000165b79d80bd6c00
dbaranchuk <dmitrybaranchuk@gmail.com>
2017-12-19 05:39:41 +0300

    .

---
+++ hnswlib/hnswlib.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifdef _MSC_VER
+#include <intrin.h>
+#include <stdexcept>
+
+#define  __builtin_popcount(t) __popcnt(t)
+
+#endif
+
+typedef unsigned int idx_t;
+
+#if defined(__GNUC__)
+#define PORTABLE_ALIGN32 __attribute__((aligned(32)))
+#else
+#define PORTABLE_ALIGN32 __declspec(align(32))
+#endif
+
+using namespace std;
+
+#include "hnswalg.h"
