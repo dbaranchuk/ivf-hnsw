0b153896162a9e76ce17768ece0af02f2cff8c43
dbaranchuk <dmitrybaranchuk@gmail.com>
2018-01-15 19:06:58 +0300

    .

--- examples/run_deep1b_grouping.sh
+++ examples/run_deep1b_grouping.sh
@@ -41,7 +41,7 @@ k="100"               # Number of the closest vertices to search
 nprobe="32"           # Number of probes at query time
 max_codes="10000"     # Max number of codes to visit to do a query
 efSearch="80"         # Max number of candidate vertices in priority queue to observe during seaching
-pruning="pruning"     # Turn on/off pruning. Options: "pruning", ""
+pruning=""     # Turn on/off pruning. Options: "pruning", ""
 
 #########
 # Paths #
