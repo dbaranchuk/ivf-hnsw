5d3f53dbadcbba26f7404265ec35912ddf0160c6
dbaranchuk <dmitrybaranchuk@gmail.com>
2018-01-17 01:29:06 +0300

    .

--- examples/run_deep1b_grouping.sh
+++ examples/run_deep1b_grouping.sh
@@ -14,7 +14,7 @@ efConstruction="500"  # Max number of candidate vertices in priority queue to ob
 nb="1000000000"       # Number of base vectors
 
 nt="256000"         # Number of learn vectors
-nsubt="128000"         # Number of learn vectors to train (random subset of the learn set)
+nsubt="256000"         # Number of learn vectors to train (random subset of the learn set)
 
 #nt="10000000"         # Number of learn vectors
 #nsubt="65536"         # Number of learn vectors to train (random subset of the learn set)
