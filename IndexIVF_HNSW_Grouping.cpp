79254574dd84d30bbbdff5161d3b2d260a0ee39e
dbaranchuk <dmitrybaranchuk@gmail.com>
2018-01-17 01:19:19 +0300

    .

--- IndexIVF_HNSW_Grouping.cpp
+++ IndexIVF_HNSW_Grouping.cpp
@@ -465,6 +465,7 @@ namespace ivfhnsw
                 }
             }
         }
+        std::cout << "Training Residual PQ codebook " << std::endl;
         std::cout << av_dist << std::endl;
 
         printf("Training %zdx%zd PQ on %ld vectors in %dD\n", pq->M, pq->ksub, train_residuals.size() / d, d);
