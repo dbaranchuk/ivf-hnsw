ea05e025960a652ddba8aba00f3cb6e81e723cb8
dbaranchuk <dmitrybaranchuk@gmail.com>
2018-01-16 21:28:12 +0300

    .

--- examples/run_deep1b_MX.sh
+++ examples/run_deep1b_MX.sh
@@ -67,7 +67,7 @@ path_index="${path_model}/ivfhnsw_PQ${code_size}_4M.index"
 #######
 # Run #
 #######
-/home/dbaranchuk/ivf-hnsw/bin/demo_ivfhnsw_deep1b -M ${M} \
+nohup /home/dbaranchuk/ivf-hnsw/bin/demo_ivfhnsw_deep1b -M ${M} \
                                                   -efConstruction ${efConstruction} \
                                                   -nb ${nb} \
                                                   -nt ${nt} \
@@ -91,4 +91,4 @@ path_index="${path_model}/ivfhnsw_PQ${code_size}_4M.index"
                                                   -path_info ${path_info} \
                                                   -path_pq ${path_pq} \
                                                   -path_norm_pq ${path_norm_pq} \
-                                                  -path_index ${path_index}
+                                                  -path_index ${path_index} > 4M.out
