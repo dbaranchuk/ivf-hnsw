9a8572957c8d9fae3489f4d39ae27dde64ea3a92
dbaranchuk <dmitrybaranchuk@gmail.com>
2018-01-21 14:44:54 +0300

    .

--- IndexIVF_HNSW.cpp
+++ IndexIVF_HNSW.cpp
@@ -11,8 +11,8 @@ namespace ivfhnsw {
         norm_pq = new faiss::ProductQuantizer(1, 1, nbits_per_idx);
 
         code_size = pq->code_size;
-        norms.resize(32768); // buffer for reconstructed base points at search time supposing that
-                             // the max size of the list is less than 32768.
+        norms.resize(65536); // buffer for reconstructed base points at search time supposing that
+                             // the max size of the list is less than 65536.
         precomputed_table.resize(pq->ksub * pq->M);
 
         codes.resize(nc);
