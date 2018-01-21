fe8083bafac39b9b7dadf8b9b4caa315350d5a63
dbaranchuk <dmitrybaranchuk@gmail.com>
2018-01-21 13:03:23 +0300

    .

--- IndexIVF_HNSW_Grouping.cpp
+++ IndexIVF_HNSW_Grouping.cpp
@@ -189,7 +189,7 @@ namespace ivfhnsw
 
                 float *subr = r.data() + i*nsubc;
                 float alpha = alphas[centroid_idx];
-                float q_c = (1 - alpha) * query_centroid_dists[centroid_idx];
+                float term1 = (1 - alpha) * query_centroid_dists[centroid_idx];
 
                 for (int subc = 0; subc < nsubc; subc++) {
                     if (subgroup_sizes[centroid_idx][subc] == 0)
@@ -203,8 +203,8 @@ namespace ivfhnsw
                         used_centroid_idxs.push_back(nn_centroid_idx);
                     }
                     // TODO: сделать красиво
-                    subr[subc] = term1 - (1 - alpha) * alpha * inter_centroid_dists[centroid_idx][subc]
-                                 + alpha * query_centroid_dists[nn_centroid_idx];
+                    subr[subc] = term1 - alpha * ((1 - alpha) * inter_centroid_dists[centroid_idx][subc]
+                                 - query_centroid_dists[nn_centroid_idx]);
                     threshold += subr[subc];
                     nsubgroups++;
                 }
