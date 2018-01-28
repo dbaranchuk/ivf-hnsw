d16e8080622cff6f95550bf86c24e0637ec608fb
dbaranchuk <dmitrybaranchuk@gmail.com>
2018-01-28 18:51:20 +0300

    .

---
+++ data/SIFT1B/load_sift1b.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+P="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo $P
+
+wget -P $P ftp://ftp.irisa.fr/local/texmex/corpus/bigann_query.bvecs.gz
+gzip -d $P/bigann_query.bvecs.gz
+
+wget -P $P ftp://ftp.irisa.fr/local/texmex/corpus/bigann_gnd.tar.gz
+tar xvzf $P/bigann_gnd.tar.gz
+
+wget -P $P ftp://ftp.irisa.fr/local/texmex/corpus/bigann_base.bvecs.gz
+gzip -d $P/bigann_base.bvecs.gz
+
+wget -P $P ftp://ftp.irisa.fr/local/texmex/corpus/bigann_learn.bvecs.gz
+gzip -d $P/bigann_learn.bvecs.gz
