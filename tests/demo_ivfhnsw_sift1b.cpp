7bc1bd89a6b04a2e7866b30ea0f9f877033623d8
dbaranchuk <dmitrybaranchuk@gmail.com>
2018-02-02 13:46:17 +0300

    .

--- tests/demo_ivfhnsw_sift1b.cpp
+++ tests/demo_ivfhnsw_sift1b.cpp
@@ -180,8 +180,9 @@ int main(int argc, char **argv)
 
     StopW stopw = StopW();
     for (size_t i = 0; i < opt.nq; i++) {
+        std::cout << "HUI\n";
         index->search(opt.k, massQ.data() + i*opt.d, distances, labels);
-
+        std::cout << "HUI\n";
         std::priority_queue<std::pair<float, idx_t >> gt(answers[i]);
         std::unordered_set<idx_t> g;
 
