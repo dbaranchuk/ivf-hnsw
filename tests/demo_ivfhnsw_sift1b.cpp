d83964dc8cef8baf82af87c1191fd952fa5aee37
dbaranchuk <dmitrybaranchuk@gmail.com>
2018-02-03 12:17:46 +0300

    .

--- tests/demo_ivfhnsw_sift1b.cpp
+++ tests/demo_ivfhnsw_sift1b.cpp
@@ -39,7 +39,7 @@ int main(int argc, char **argv)
     readXvecFvec<uint8_t>(query_input, massQ.data(), opt.d, opt.nq);
     query_input.close();
 
-    for (int i = 1024; i < 1152; i++){
+    for (int i = 1024; i < 1536; i++){
         std::cout << massQ[i] << " ";
     }
 
