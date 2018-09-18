f84925da4b00d2bde57334e6e3365805c15bbf22
Borodin Gregory <grihabor@gmail.com>
2018-09-18 09:13:38 +0000

    Add basic python test

--- python-tests/test_wrapper.py
+++ python-tests/test_wrapper.py
@@ -1,4 +1,15 @@
-def test_wrapper():
+def test_lowlevel_constructor_and_destructor_wrappers():
     from ivfhnsw import _wrapper
     i = _wrapper.new_IndexIVF_HNSW(4,4,4,4)
     _wrapper.delete_IndexIVF_HNSW(i)
+
+
+def test_pipeline():
+    from ivfhnsw import Index
+    index = Index(4,4,4,4)
+    index.build_quantizer('', '', '')
+    index.assign([[5,5,5,5]], 2)
+    distances, labels = index.search([[1,2,3,4],
+                                      [6,2,3,2]], 3)
+    assert distances.shape[0] == 3
+    assert labels.shape[0] == 3
