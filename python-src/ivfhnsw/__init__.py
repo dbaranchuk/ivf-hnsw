9b784829755a1131c375f7b9268f9c488bd3224a
Borodin Gregory <grihabor@gmail.com>
2018-09-18 10:00:01 +0000

    Add Proxy class for IndexIVF_HNSW_Grouping

--- python-src/ivfhnsw/__init__.py
+++ python-src/ivfhnsw/__init__.py
@@ -1,5 +1,7 @@
 from .index import Index
+from .index_grouping import IndexGrouping
 
 __all__ = (
     'Index',
+    'IndexGrouping',
 )
---
+++ python-src/ivfhnsw/index_grouping.py
@@ -0,0 +1,6 @@
+from .wrapper import IndexIVF_HNSW_Grouping
+from .index import Index
+
+
+class IndexGrouping(IndexIVF_HNSW_Grouping, Index):
+    pass
